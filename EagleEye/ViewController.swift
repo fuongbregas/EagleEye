ff85f883052f1cb555327e2df51472d2275b71f3
Fuong <fuongbregas@icloud.com>
2018-08-19 22:27:06 -0700

    Added start new round and new game
    Everytime player submits his prediction, new round starts.
    If player want to reset everything, clicks Start Over, new game starts then new round begins

--- EagleEye/ViewController.swift
+++ EagleEye/ViewController.swift
@@ -11,14 +11,48 @@ import UIKit
 class ViewController: UIViewController {
     var currentValue: Int = 50
     var targetValue: Int = 50
+    var score: Int = 0
+    var round: Int = 0
     
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    // Every time player submits, new round starts
+    func startNewRound(){
+        currentValue = 50
+        targetValue = 1 + Int(arc4random_uniform(100))
+        slider.value = Float(currentValue)
+        round = round + 1
+    }
+    
+    // When player wants to start over
+    func startNewGame(){
+        round = 0
+        score = 0
+    }
+    
+    @IBAction func startOver(){
+        startNewGame()
+        startNewRound()
+        updateLabel()
+    }
+    
+    func updateLabel(){
+        targetLabel.text = String(targetValue) // Convert target value to string, then set it to target label
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startNewRound()
+        updateLabel()
+        /*
         currentValue = lroundf(slider.value)
         targetValue = 1 + Int(arc4random_uniform(100))
+        */
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,15 +61,40 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAction(){
-        let message = "The value of the sider is: \(currentValue)\n" + "The target value is: \(targetValue)"
+        let differences = abs(targetValue - currentValue)
+        var points = 100 - differences // The lower differences, the higher points
         
-        let alert = UIAlertController(title: "Result", message: message, preferredStyle: .alert)
+        
+        let title: String
+        if differences == 0{
+            title = "You have Eagle Eyes!"
+            points = points + 100
+        }
+        else if differences < 5 {
+            title = "It was close!"
+            points = points + 50
+        }
+        else if differences < 10 {
+            title = "Please check you eyes"
+        }
+        else{
+            title = "You are BLIND!"
+        }
+        score = score + points // Total points
+        
+        let message = "The value of the sider is: \(currentValue)\n" + "The target value is: \(targetValue)\n" + "You scored \(points) points!"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Lmao", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        // Start new round after player submit value so that new random target value can be generate
+        startNewRound()
+        updateLabel()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider){
